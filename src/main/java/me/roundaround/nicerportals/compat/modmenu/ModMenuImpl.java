4ee5dea1e8368e3bd6c2f242075e30019a528916
Evan Steinkerchner <esteinkerchner@gmail.com>
2022-08-14 11:37:44 -0400

    Update to roundalib 0.2.1

--- src/main/java/me/roundaround/nicerportals/compat/modmenu/ModMenuImpl.java
+++ src/main/java/me/roundaround/nicerportals/compat/modmenu/ModMenuImpl.java
@@ -4,11 +4,11 @@ import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
 
 import me.roundaround.nicerportals.NicerPortalsMod;
-import me.roundaround.roundalib.config.gui.ConfigScreen;
+import me.roundaround.roundalib.config.gui.screen.ConfigScreen;
 
 public class ModMenuImpl implements ModMenuApi {
-    @Override
-    public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        return (screen) -> new ConfigScreen(screen, NicerPortalsMod.CONFIG);
-    }
+  @Override
+  public ConfigScreenFactory<?> getModConfigScreenFactory() {
+    return (screen) -> new ConfigScreen(screen, NicerPortalsMod.CONFIG);
+  }
 }
