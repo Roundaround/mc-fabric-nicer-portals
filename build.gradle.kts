bd38d3c98802b07968fab8fc64ecaabc8dcbd471
Evan Steinkerchner <esteinkerchner@gmail.com>
2023-05-02 22:20:29 -0400

    Update plugin, add plugins to deps for debugging

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,15 @@
 plugins {
-  id("roundalib") version "0.2.52"
+  id("roundalib") version "0.2.65"
+}
+
+repositories {
+  mavenCentral()
+  gradlePluginPortal()
+  maven("https://maven.fabricmc.net/")
+  maven("https://maven.rnda.dev/releases/")
+}
+
+dependencies {
+  implementation("fabric-loom", "fabric-loom.gradle.plugin", "1.1-SNAPSHOT")
+  implementation("roundalib", "roundalib.gradle.plugin", "0.2.65")
 }
