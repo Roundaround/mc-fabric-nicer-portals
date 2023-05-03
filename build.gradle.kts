9afb786567b13ee0503564e3f7a01912243f39f8
Evan Steinkerchner <esteinkerchner@gmail.com>
2023-05-02 23:35:48 -0400

    Remove plugin deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,3 @@
 plugins {
   id("roundalib") version "0.2.65"
 }
-
-repositories {
-  mavenCentral()
-  gradlePluginPortal()
-  maven("https://maven.fabricmc.net/")
-  maven("https://maven.rnda.dev/releases/")
-}
-
-dependencies {
-  implementation("fabric-loom", "fabric-loom.gradle.plugin", "1.1-SNAPSHOT")
-  implementation("roundalib", "roundalib.gradle.plugin", "0.2.65")
-}
